5dc3f3937b25917160fe4adf6d99dd3826d85875
Alex Darvin <amdarvin14@gmail.com>
2020-10-19 20:10:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,5 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :in_business
 end
