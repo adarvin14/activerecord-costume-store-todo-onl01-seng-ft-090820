a72b985221f8e2b44ae47753ae964c9b0cd8974b
Alex Darvin <amdarvin14@gmail.com>
2020-10-19 19:51:01 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name
       t.float :price
       t.string :image_url
-      t.integer :size 
+      t.string :size 
       t.timestamp :created_at
       t.timestamp :updated_at
     end
