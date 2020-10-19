87c3f6a24de65a434a314585be6eb503d8def57d
Alex Darvin <amdarvin14@gmail.com>
2020-10-19 20:09:57 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,4 +2,7 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :
 end
